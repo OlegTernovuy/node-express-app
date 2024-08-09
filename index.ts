add13ea9726171ebbd6141468fc3fe95b34753f6
Oleg Ternovuy <ternovuyo079@gmail.com>
2024-08-09 10:09:09 +0300

    add swagger

--- index.ts
+++ index.ts
@@ -1,4 +1,6 @@
 import express, { Application } from 'express';
+import swaggerUi from 'swagger-ui-express';
+import swaggetJsdoc from 'swagger-jsdoc';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
@@ -10,12 +12,36 @@ dotenv.config();
 const app: Application = express();
 const port = process.env.PORT || 4200;
 
+const swaggerUICss =
+    'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.3.0/swagger-ui.min.css';
+
+const options = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Docs',
+            version: '1.0.0',
+            description: 'Documentation for axels-loki-api',
+        },
+        servers: [{ url: 'https://loki-api2.axels.com.ua/api' }],
+    },
+    apis: ['src/routes/*.ts'],
+};
+
+const swaggerSpec = swaggetJsdoc(options);
+
 mongoose
     .connect(process.env.MONGO_URL as string)
     .then(() => console.log('MongoDB conected successfully'))
     .catch((err) => console.log('MongoDB conected failed', err));
 
 app.use(express.json());
+
+app.use(
+    '/docs',
+    swaggerUi.serve,
+    swaggerUi.setup(swaggerSpec, { customCssUrl: swaggerUICss })
+);
 
 app.use('/countries', countryRoutes);
 

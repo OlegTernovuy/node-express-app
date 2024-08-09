d0acb1553e414e7d260cafca6efa0fd2ac7643bd
Oleg Ternovuy <ternovuyo079@gmail.com>
2024-08-09 12:31:28 +0300

    delete custom style

--- index.ts
+++ index.ts
@@ -12,8 +12,8 @@ dotenv.config();
 const app: Application = express();
 const port = process.env.PORT || 4200;
 
-const swaggerUICss =
-    'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.3.0/swagger-ui.min.css';
+// const swaggerUICss =
+//     'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.3.0/swagger-ui.min.css';
 
 mongoose
     .connect(process.env.MONGO_URL as string)
@@ -25,11 +25,7 @@ app.use(express.json());
 app.use(
     '/api-docs',
     swaggerUi.serve,
-    swaggerUi.setup(swaggerSpec, {
-        customCss:
-            '.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }',
-        customCssUrl: swaggerUICss,
-    })
+    swaggerUi.setup(swaggerSpec)
 );
 
 app.use('/countries', countryRoutes);

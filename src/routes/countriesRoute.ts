0353166eec77ef44714ee7f427c65718e59be7d0
Oleg Ternovuy <ternovuyo079@gmail.com>
2024-08-09 10:41:26 +0300

    move swagger docs to routes

--- src/routes/countriesRoute.ts
+++ src/routes/countriesRoute.ts
@@ -15,6 +15,41 @@ import { upload } from '../utils/imageStorege';
 
 const countryRoutes = Router();
 
+/**
+ * @openapi
+ * paths:
+ *   /countries/countryWithDishes:
+ *     post:
+ *       tags:
+ *         - Auth
+ *       summary: Register a user
+ *       description: Create a new user and default set status disabled
+ *       requestBody:
+ *           required: true
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 type: object
+ *                 properties:
+ *                   email:
+ *                     type: string
+ *                     example: email@test.com
+ *                   password:
+ *                     type: string
+ *                     example: password
+ *       responses:
+ *         201:
+ *           description: OK
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 type: object
+ *                 properties:
+ *                   message:
+ *                     type: string
+ *                     example: User registered successfully. Awaiting approval      
+ */
+
 countryRoutes.get('/', getCountries);
 
 countryRoutes.get('/countryWithDishes', countryWithDishes);

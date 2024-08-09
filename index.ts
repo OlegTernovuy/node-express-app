492ff53ae19aeb437a6c85dce61281f123d86a6f
Oleg Ternovuy <ternovuyo079@gmail.com>
2024-08-09 13:25:54 +0300

    change style

--- index.ts
+++ index.ts
@@ -7,13 +7,16 @@ import { countryRoutes, dishesRoutes } from './src/routes/index';
 import errorHandler from './src/middlewares/errorHandler';
 import swaggerSpec from './src/utils/swagger';
 
+const swaggerUICss =
+    'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.4.0/swagger-ui.min.css';
+
+const customCss =
+    '.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }';
+
 dotenv.config();
 
 const app: Application = express();
 const port = process.env.PORT || 4200;
-
-const swaggerUICss =
-    'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.4.0/swagger-ui.min.css';
 
 mongoose
     .connect(process.env.MONGO_URL as string)
@@ -26,9 +29,7 @@ app.use(
     '/api-docs',
     swaggerUi.serve,
     swaggerUi.setup(swaggerSpec, {
-        customCss:
-            '.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }',
-        customCssUrl: swaggerUICss,
+        customCss: swaggerUICss,
     })
 );
 
---
+++ public/style/swagger-ui.ts
@@ -0,0 +1,7 @@
+const swaggerUICss =
+    'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.4.0/swagger-ui.min.css';
+
+const customCss =
+    '.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }';
+
+export { swaggerUICss, customCss };

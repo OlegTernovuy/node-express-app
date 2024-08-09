de60319544edf816167ff11efb006ee58cb51f25
Oleg Ternovuy <ternovuyo079@gmail.com>
2024-08-09 11:27:43 +0300

    fix swagger.ts

--- src/routes/countrySwagger.ts
+++ src/routes/countrySwagger.ts
@@ -1,33 +1,16 @@
 /**
- * @openapi
- *   /auth/register:
- *     post:
- *       tags:
- *         - Auth
- *       summary: Register a user
- *       description: Create a new user and default set status disabled
- *       requestBody:
- *           required: true
- *           content:
- *             application/json:
- *               schema:
- *                 type: object
- *                 properties:
- *                   email:
- *                     type: string
- *                     example: email@test.com
- *                   password:
- *                     type: string
- *                     example: password
- *       responses:
- *         201:
- *           description: OK
- *           content:
- *             application/json:
- *               schema:
- *                 type: object
- *                 properties:
- *                   message:
- *                     type: string
- *                     example: User registered successfully. Awaiting approval
+ * @swagger
+ * /books:
+ *   get:
+ *     summary: Returns all books
+ *     tags: [Books]
+ *     responses:
+ *       200:
+ *         description: the list of books
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Book'
  */
--- src/utils/swagger.ts
+++ src/utils/swagger.ts
@@ -1,6 +1,56 @@
 import swaggerJsDoc from 'swagger-jsdoc';
 import path from 'path';
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Book:
+ *       type: object
+ *       required:
+ *         - title
+ *         - author
+ *         - published
+ *       properties:
+ *         title:
+ *           type: string
+ *           description: Title of the book
+ *         author:
+ *           type: string
+ *           descripton: Name of the author of the book
+ *         published:
+ *           type: boolean
+ *           descripton: If the book is published or not
+ *       example:
+ *         title: A great book
+ *         author: John
+ *         published: true
+ *     BookUpdate:
+ *       type: object
+ *       optional:
+ *         - title
+ *         - author
+ *         - published
+ *       properties:
+ *         title:
+ *           type: string
+ *           description: Title of the book
+ *         author:
+ *           type: string
+ *           descripton: Name of the author of the book
+ *         published:
+ *           type: boolean
+ *           descripton: If the book is published or not
+ *       example:
+ *         title: An updated book title
+ *         author: A new author
+ *         published: true
+ *
+ * @swagger
+ *  tags:
+ *    name: Books
+ */
+
 const swaggerSpec = swaggerJsDoc({
     definition: {
         openapi: '3.0.0',
